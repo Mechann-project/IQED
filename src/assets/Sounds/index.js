d3008df44b345cff2291c57675f9d76d668d4f4e
Puviyarasu <puviyarasu5341@gmail.com>
2024-12-02 10:37:03 +0530

    sss

--- src/assets/Sounds/index.js
+++
@@ -1,6 +0,0 @@
-import countdownSound from "./countdown.mp3";
-import letsGoSound from "./letsGoSound.mp3";
-import MotSound from "./MotSound.mp3";
-
-
-export { letsGoSound, countdownSound, MotSound };
--- src/assets/index.js
+++
@@ -1,3 +0,0 @@
-export * from './Image'
-export * from './SVG'
-export * from './Sounds'
