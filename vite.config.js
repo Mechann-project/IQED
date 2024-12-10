0e9f2b1415faad3bd6339c7db5286a337c5a9c22
Puviyarasu <puviyarasu5341@gmail.com>
2024-12-10 16:53:34 +0530

    kdjf

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 import removeConsole from 'vite-plugin-remove-console';
 
 export default defineConfig({
-  base: "/",
   plugins: [
     react(),
     removeConsole(), 
