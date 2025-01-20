bd142e4786276b89fa5030dadeba3f8f214148e7
gowthamrajvp <gowthamraj9344@gmail.com>
2025-01-20 11:12:59 +0530

    Q

--- src/Pages/Layout/RootLayout.jsx
+++ src/Pages/Layout/RootLayout.jsx
@@ -1,13 +1,12 @@
 import { useMemo } from "react";
 import { Outlet, useLocation } from "react-router-dom";
 import { DynamicBackground } from "../../Common";
-import {
-  WhiteBackgroundSVG,
-  YellowBackgroundSVG,
-  BlueBackgroundSVG,
-} from "../../assets/SVG";
+import { YellowBackgroundSVG, BlueBackgroundSVG } from "../../assets/SVG";
 import { Toaster } from "react-hot-toast";
 // import { SocketProvider } from "../../Socket/SocketContext";
+
+const DEFAULT_BACKGROUND_URL =
+  "https://firebasestorage.googleapis.com/v0/b/eshikhay-database.appspot.com/o/background%2FWhiteLineBG.svg?alt=media&token=0492f27f-f5d6-4e53-8bd0-92d080d3c131";
 
 const RootLayout = () => {
   const location = useLocation();
@@ -21,23 +20,16 @@ const RootLayout = () => {
 
     const path = location.pathname.toLowerCase();
 
-    if (
-      path.startsWith("/iqquiz") ||
-      path.startsWith("/quiz") ||
-      path.startsWith("/match")
-    ) {
-      return YellowBackgroundSVG;
-    }
-    if (path.endsWith("/result")) {
+    if (path.startsWith("/iqquiz") || path.startsWith("/quiz") || path.startsWith("/match")) {
       return YellowBackgroundSVG;
     }
 
-    return backgroundMap[path] ;
+    return backgroundMap[path] || DEFAULT_BACKGROUND_URL;
   }, [location.pathname]);
 
   return (
     <DynamicBackground
-    sx={{ backgroundImage: `url(${backgroundImage || WhiteBackgroundSVG})` }}
+      sx={{ backgroundImage: `url(${backgroundImage})` }}
       className="Root-BackGround"
     >
       <Outlet />
